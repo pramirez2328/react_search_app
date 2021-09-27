ef4afce363334409e6f9981e596548b8f7a0c528
ramirez <prlara2000@gmail.com>
2021-09-27 16:59:33 -0500

    modify the App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import List from "./List";
 import Search from "./search";
 import stories from "./stories";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState(
+    localStorage.getItem("previous") || ""
+  );
   const [current, setCurrent] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("previous", searchTerm);
+  }, [searchTerm]);
 
   const handleChange = (event) => {
     let cond = event.target.value;
     setSearchTerm(cond);
+
     const filterItem = stories.filter((i) => {
       return i.title.toLowerCase().includes(cond) || i.title.includes(cond);
     });
